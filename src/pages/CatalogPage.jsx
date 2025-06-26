51efcedf50ac372699cf268b5920ca1b9c3a8bf4
Atilla.G <atillagoguslu@hotmail.com>
2025-06-26 21:01:08 +0300

    Basic design implemented.

---
+++ src/pages/CatalogPage.jsx
@@ -0,0 +1,13 @@
+import styles from './CatalogPage.module.css';
+
+const CatalogPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1>Catalog</h1>
+      </div>
+    </div>
+  );
+};
+
+export default CatalogPage;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import styles from './HomePage.module.css';
+
+const HomePage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1>HomePage </h1>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
