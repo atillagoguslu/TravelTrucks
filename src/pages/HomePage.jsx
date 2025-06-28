39b9dea24245948f48e7dc102c2808d9b7ea5292
Atilla.G <atillagoguslu@hotmail.com>
2025-06-28 22:10:28 +0300

    Hero -> View Now button fixed.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,19 @@
 import styles from './HomePage.module.css';
+import { useNavigate } from 'react-router';
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleViewNow = () => {
+    navigate('/catalog');
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.heroTitleContainer}>
         <h1 className={styles.heroTitle}>Campers of your dreams</h1>
         <p className={styles.heroDescription}>You can find everything you want in our catalog</p>
-        <button className={styles.heroButton}>
+        <button className={styles.heroButton} onClick={handleViewNow}>
           <span className={styles.heroButtonText}>View Now</span>
         </button>
       </div>
