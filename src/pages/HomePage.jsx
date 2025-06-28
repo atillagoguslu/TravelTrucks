01c18a01f9ac29e2201f195f29794685a7e1e2de
Atilla.G <atillagoguslu@hotmail.com>
2025-06-28 21:37:03 +0300

    Hero title and button layout added.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,9 +3,13 @@ import styles from './HomePage.module.css';
 const HomePage = () => {
   return (
     <div className={styles.container}>
-        <h1>HomePage 2</h1>
+      <div className={styles.heroTitleContainer}>
+        <h1 className={styles.heroTitle}>Campers of your dreams</h1>
+        <p className={styles.heroDescription}>You can find everything you want in our catalog</p>
+        <button className={styles.heroButton}>View Now</button>
+      </div>
     </div>
   );
 };
-
+ 
 export default HomePage;
