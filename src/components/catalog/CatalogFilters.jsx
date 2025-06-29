e1c3e8f22fd7e058ff21fef0a3e89ad649c16b4c
Atilla.G <atillagoguslu@hotmail.com>
2025-06-29 21:48:07 +0300

    CatalogPage created.

---
+++ src/components/catalog/CatalogFilters.jsx
@@ -0,0 +1,14 @@
+import style from "./CatalogFilters.module.css"
+
+const CatalogFilters = () => {
+  
+  return (
+    <div className="style.container">
+      
+
+    </div>
+    
+)
+}
+
+export default CatalogFilters;
--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -3,8 +3,9 @@ import styles from './CatalogPage.module.css';
 const CatalogPage = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <h1>Catalog</h1>
+      <div className={styles.leftSide}>
+      </div>
+      <div className={styles.rightSide}>
       </div>
     </div>
   );
