013e6cf146feccfd603787d50d1a6411eede22f0
Atilla.G <atillagoguslu@hotmail.com>
2025-06-30 21:47:10 +0300

    CatalogFilters pre-implemented.

--- src/components/catalog/CatalogFilters.jsx
+++ src/components/catalog/CatalogFilters.jsx
@@ -3,8 +3,19 @@ import style from "./CatalogFilters.module.css"
 const CatalogFilters = () => {
   
   return (
-    <div className="style.container">
-      
+    <div className={style.container}>
+      <div className={style.location}>
+        <p>Location</p>
+
+      </div>
+      <div className={style.vehicleEquipment}>
+        <h2>Vehicle equipment</h2>
+
+      </div>
+      <div className={style.vehicleType}>
+        
+      </div>
+      <button className={style.searchButton}>Search</button>
 
     </div>
     
