af1a3d8f36b8383bbf034f3e0d670c72ee3ae9dc
Atilla.G <atillagoguslu@hotmail.com>
2025-07-01 16:14:45 +0300

    Favs added to store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import trucksReducer from "./trucks/slice.js";
+import favouritesReducer from "./favourites/slice.js";
 
 const store = configureStore({
   reducer: {
     trucks: trucksReducer,
-  }
-})
+    favourites: favouritesReducer,
+  },
+});
 
 export default store;
