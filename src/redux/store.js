2707a501d1f4975bc8b90e5056875342ca9affc9
Atilla.G <atillagoguslu@hotmail.com>
2025-07-01 15:55:37 +0300

    Reduc Store is ready for get Trucks.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import trucksReducer from "./trucks/slice.js";
 
+const store = configureStore({
+  reducer: {
+    trucks: trucksReducer,
+  }
+})
+
+export default store;
