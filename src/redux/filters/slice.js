faa299dcba63bbc993cbcfa9bf773a447450f0ec
Atilla Göğüslü <atillagoguslu@hotmail.com>
2025-07-02 19:52:56 +0300

    Filter Slice implemented.

--- src/redux/filters/slice.js
+++ src/redux/filters/slice.js
@@ -1,1 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  allFilters: [],
+};
+
+const filterSlice = createSlice({
+  name: "filters",
+  initialState,
+  reducers: {
+    addFilters: (state, action) => {
+      state.allFilters.push(action.payload);
+    },
+    
+  },
+});
+
+export const { addFilters } = filterSlice.actions;
+export default filterSlice.reducer;
