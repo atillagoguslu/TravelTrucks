cb21e83cf1d026cf6280afebe293ce01dddf7c09
Atilla Göğüslü <atillagoguslu@hotmail.com>
2025-07-07 21:44:24 +0300

    Redux Persist implemented.

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router';
-import HomePage from './pages/HomePage';
-import CatalogPage from './pages/CatalogPage';
-import { Provider } from 'react-redux';
-import store from './redux/store.js';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import CatalogPage from "./pages/CatalogPage";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./redux/store.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/catalog" element={<CatalogPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/catalog" element={<CatalogPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,44 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import trucksReducer from "./trucks/slice.js";
 import favouritesReducer from "./favourites/slice.js";
 import filtersReducer from "./filters/slice.js";
 
+
+const filtersPersistConfig = {
+  key: "filters",
+  storage,
+  whitelist: ["activeFilters", "pagination"],
+};
+
+const favouritesPersistConfig = {
+  key: "favourites",
+  storage,
+};
+
+const persistedFiltersReducer = persistReducer(
+  filtersPersistConfig,
+  filtersReducer
+);
+const persistedFavouritesReducer = persistReducer(
+  favouritesPersistConfig,
+  favouritesReducer
+);
+
 const store = configureStore({
   reducer: {
     trucks: trucksReducer,
-    favourites: favouritesReducer,
-    filters: filtersReducer,
+    favourites: persistedFavouritesReducer,
+    filters: persistedFiltersReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
 });
 
+export const persistor = persistStore(store);
 export default store;
