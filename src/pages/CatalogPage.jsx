55651b9d2cc1439d978dcc6cbdb5968794e9ebb6
Atilla Göğüslü <atillagoguslu@hotmail.com>
2025-07-10 16:35:54 +0300

    Linter errors fixed.

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,35 +1,27 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import style from "./CatalogPage.module.css";
-import CatalogFilters from "../components/catalog/CatalogFilters.jsx";
-import TrucksList from "../components/catalog/TrucksList.jsx";
-import {
-  selectIsLoadingFilters,
-  selectTrucksErrors,
-} from "../redux/trucks/selectors.js";
-import {
-  selectHasActiveFilters,
-  selectActiveFilters,
-  selectPagination,
-} from "../redux/filters/selectors.js";
-import { fetchTrucksWithFilters } from "../redux/trucks/operations.js";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import style from './CatalogPage.module.css';
+import CatalogFilters from '../components/catalog/CatalogFilters.jsx';
+import TrucksList from '../components/catalog/TrucksList.jsx';
+import { selectIsLoadingFilters, selectTrucksErrors } from '../redux/trucks/selectors.js';
+import { selectActiveFilters } from '../redux/filters/selectors.js';
+import { fetchTrucksWithFilters } from '../redux/trucks/operations.js';
 
 const CatalogPage = () => {
   const dispatch = useDispatch();
 
   const isLoadingFilters = useSelector(selectIsLoadingFilters);
   const error = useSelector(selectTrucksErrors);
-  const hasActiveFilters = useSelector(selectHasActiveFilters);
   const activeFilters = useSelector(selectActiveFilters);
-  const pagination = useSelector(selectPagination);
 
   useEffect(() => {
     dispatch(
       fetchTrucksWithFilters({
         filters: activeFilters,
         pagination: { currentPage: 1, limit: 4 },
-      })
+      }),
     );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch]);
 
   if (isLoadingFilters) {
